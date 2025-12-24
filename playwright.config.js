65a71913b3202156e639a553a86c583419bf66c0
AmbarishDash15 <122718248+AmbarishDash15@users.noreply.github.com>
2025-12-24 19:35:02 +1100

    allure report

--- playwright.config.js
+++ playwright.config.js
@@ -26,7 +26,8 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   //workers: process.env.CI ? 5 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  //reporter: 'html',
+  reporter: [["line"], ["allure-playwright"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     //browserName : 'chromium'
